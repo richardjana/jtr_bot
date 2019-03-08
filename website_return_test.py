1a7029cc7693d6627e0a209c86adcfbd5e7f230f
richardjana <richard.jana@kit.edu>
2019-03-08 15:29:08 +0100

    .read() gives string, .split() turns into words, which can be looped over. don't call read() multiple times though

--- website_return_test.py
+++ website_return_test.py
@@ -1,44 +1,28 @@
-import os
-import numpy as np
-
-import time
-import datetime
-import pytz
-
-import webbrowser
-import mechanize
-
-import smtplib
-import imaplib
-import email
-
 tournament_id = 445
-team_id = 3
 email = 'jtr.python@gmail.com'
 
-url = 'https://turniere.jugger.org/tournament.signin.php?id='+str(tournament_id)
+url = 'https://turniere.jugger.org/tournament.php?id='+str(tournament_id)
+
+import mechanize
+import webbrowser
+
+
 br = mechanize.Browser()
 br.set_handle_robots(False) # ignore robots
 
 # solve captcha
 response = br.open(url)
-with open('results.html','w') as f:
-    f.write(response.read())
-    
-infile = open('results.html','r') # besser machen ohne file input output
-d = infile.readlines()
-infile.close()
-os.remove('results.html')
-for i in range(len(d)):
-    #try:
-    #    if d[i].split()[2] == 'name="control_val"': # not sure what control_val is used for; maybe put into logfile?
-    #        print(d[i].split()[3][7:-1])
-    #except:
-    #    continue
+
+# response.read() == string
+# keine line breaks drin (\n oder so) -> mist, brauche ich um wenigstens etwas zu parsen? .split() geht trotzdem
+response_words = response.read().split()
+
+target_string = 'name="google-site-verification"'
+for i in range(len(response_words)):
+    #print(response_words[i])
     try:
-        if d[i].split()[7] == 'name="control"':
-            captcha_value = solve_captcha(d[i].split()[0],d[i].split()[1],d[i].split()[2])
+        if response_words[i] == target_string:
+            print(response_words[i+1])
             break
     except:
         continue
-        
